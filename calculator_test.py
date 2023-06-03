1c89c963114e905de05e9b9da045015aba33fa29
alpezel <allistairevillaruel@gmail.com>
2023-06-03 21:13:11 +0800

    FINALIZE (add pseudocode and change calc to nums)

--- calculator_test.py
+++ calculator_test.py
@@ -1,11 +1,14 @@
-from calculator_with_class import Calculator 
+# remove from calculator_with_class import Calculator
 from calculator_with_class import Display
+# imported Numbers class and change the calculator class
 from inheritance_calcu import Numbers
+
 boarder = "\n\033[0;39m==================================================="
 # Create instances of the classes
-calc = Calculator()
 menu = Display()
+# remove calc = Calculator(), since its in the Numbers class
 nums = Numbers()
+
 while True:
     menu.show_menu()
     choice = menu.get_choice()
@@ -13,17 +16,18 @@ while True:
     
     # Use if statements to check the choices and call the corresponding function 
     # Display result
+    # change calc. to nums. to apply inheritance 
     if choice == 1:
-        result = calc.Add(n1, n2)
-        
+        result = nums.Add(n1, n2)
     elif choice == 2:
-        result = calc.Subtract(n1, n2)
+        result = nums.Subtract(n1, n2)
     elif choice == 3:
-        result = calc.Multiply(n1, n2)
+        result = nums.Multiply(n1, n2)
     elif choice == 4:
-        result = calc.Divide(n1, n2)
+        result = nums.Divide(n1, n2)
 
     print("\n\033[0;36mResult:", result,"\n")
+    # pront the sign of the inputted number
     sign = nums.Sign(n1,n2)
     print(boarder)
 
--- inheritance_calcu.py
+++ inheritance_calcu.py
@@ -4,18 +4,18 @@ class Numbers(Calculator):
 
     def Sign (self, n1,n2):
         if n1 > 0:
-            print("\033[0;36mThe first number:","\033[0;39m",n1,"\033[0;36mis \033[0;46mPOSITIVE\033[0;36m")
+            print("\033[0;36mThe first number:","\033[0;39m"+ str(n1),"\033[0;36mis [ \033[0;45mPOSITIVE\033[0;36m ]")
         elif n1 < 0:
-            print("\033[0;36mThe first number:","\033[0;39m",n1,"\033[0;36mis \033[0;41mNEGATIVE\033[0;39m")
+            print("\033[0;36mThe first number:","\033[0;39m"+ str(n1),"\033[0;36mis [ \033[0;41mNEGATIVE\033[0;36m ]")
         elif n1 == 0:
-            print("\033[0;36mThe first number:","\033[0;39m",n1,"\033[0;36mis neither \033[0;46mPOSITIVE \033[0;36mnor \033[0;41mNEGATIVE\033[0;39m")
+            print("\033[0;36mThe first number:","\033[0;39m"+ str(n1),"\033[0;36mis [ neither \033[0;45mPOSITIVE\033[0;49m\033[0;36m nor \033[0;41mNEGATIVE\033[0;36m ]")
         
         if n2 > 0:
-            print("\033[0;36mThe second number:","\033[0;39m",n2,"\033[0;36mis \033[0;46mPOSITIVE\033[0;36m")
+            print("\033[0;36mThe second number:","\033[0;39m"+ str(n2),"\033[0;36mis [ \033[0;45mPOSITIVE\033[0;36m ]")
         elif n2 < 0:
-            print("\033[0;36mThe second number:","\033[0;39m",n2,"\033[0;36mis \033[0;41mNEGATIVE\033[0;39m")
+            print("\033[0;36mThe second number:","\033[0;39m"+ str(n2),"\033[0;36mis [ \033[0;41mNEGATIVE\033[0;36m ]")
         elif n2 == 0:
-            print("\033[0;36mThe second number:","\033[0;39m",n2,"\033[0;36mis neither \033[0;46mPOSITIVE \033[0;36mnor \033[0;41mNEGATIVE\033[0;39m")
+            print("\033[0;36mThe second number:","\033[0;39m"+ str(n2),"\033[0;36mis [ neither \033[0;45mPOSITIVE\033[0;49m\033[0;36m nor \033[0;41mNEGATIVE\033[0;36m ]")
         
 
         
